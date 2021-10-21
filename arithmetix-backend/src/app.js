fa7a896b1820f6b3cb7064163758cff82b7fab22
toniu <zhac032@live.rhul.ac.uk>
2021-10-21 01:41:26 +0100

    User authentication on backend.

--- arithmetix-backend/src/app.js
+++ arithmetix-backend/src/app.js
@@ -1,15 +1,17 @@
 'use strict';
+/* Required modules */
 const express = require('express');
 const config = require('./config');
-
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const formidable = require('formidable');
-const fs = require('fs');
 
 const DB = require('./db');
 const mail = require('./mail');
-const {body, validationResult} = require('express-validator');
+const {body, evResult} = require('express-validator');
+
+/* Tables of the arithmetix database,
+order based on populate.sql to avoid errors,
+related to primary/foriegn keys */
 
 const tables = [
     'users',
@@ -32,6 +34,44 @@ const allowCD = function (req, res, next) {
 app.use(allowCD);
 
 /* Router requests */
+
+/**
+ * User authentication
+ * Returns informations based on the user's role (student, teacher or admin)
+ */
+router.post(
+  '/login',
+  /* Check if the email is actually an email */
+  body('email').isEmail(),
+  /* Password must be minimum 6 characters long */
+  body('password').isLength({min: 6}),
+  async (req, res) => {
+    /* Call function to find any validation errors using Express validator */
+    const errorsFound = evResult(req);
+    if (!errorsFound.isEmpty()) {
+      console.log({errorsFound: errorsFound.array()});
+      return res.status(400).json({errorsFound: errorsFound.array()});
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    /* Create new JSON web token */
+    const token = jwt.sign({id: email},
+       config.secret,
+       {expiresIn: 86400}
+    );
+
+    /* Values for client's local storage */
+    res.status(200).send({
+      auth: true,
+      token: token,
+      user: email,
+    });
+  }
+)
+
+/* ---- */
 
 app.use(router);
 
--- arithmetix-backend/src/db.js
+++ arithmetix-backend/src/db.js
@@ -23,14 +23,33 @@ class Db {
 
     async getMethodExample(param) {
         const {rows} = await pool.query(
-        `
-        SELECT 
-        attribute
-        FROM table1
-        WHERE condition = $1`,
-        [param],
-    );
-    return rows;
+            `
+            SELECT 
+            attribute
+            FROM table1
+            WHERE condition = $1`,
+            [param],
+        );
+        return rows;
+    }
+
+    /**
+     * User authentication which checks the users credentials
+     * against credientials stored in the table for users in the database
+     * @param {String} email - Used to uniquely identify the user
+     * @param {String} password - Used for user authentication
+     * @returns {Boolean} Returns if the query has any existing rows
+     */
+    async login(email, password) {
+        const {rows} = await pool.query(
+            `SELECT
+             *
+            FROM users 
+            WHERE email = $1
+            AND password=crypt($2, password)`,
+            [email, password],
+        );
+        return !!rows.length;
     }
 }
 
