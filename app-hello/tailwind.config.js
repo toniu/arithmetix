38389f9a31839ff8fcdd2f49c12d035c58a44835
toniu <zhac032@live.rhul.ac.uk>
2021-11-14 17:48:05 +0000

    Refactoring frontend folder part I.

---
+++ app-hello/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.html', './src/**/*.vue', './src/*.js', './src/*.vue'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
